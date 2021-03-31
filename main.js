2f42626b4b4d9246932a99515d566ba0cd37fdc1
Laura <65158950+lauratro@users.noreply.github.com>
2021-03-31 16:37:03 +0200

    First NavBar example

---
+++ main.js
@@ -0,0 +1,10 @@
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".menu-list");
+const links = document.querySelectorAll(".list-elem");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+  links.forEach((link) => {
+    link.classList.toggle("end");
+  });
+});
